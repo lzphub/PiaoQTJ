881a2b8ad892373f822bc0cf9f53b0481412e05e
qs <425570976@qq.com>
2018-10-31 09:47:10 +0800

    模块

---
+++ module-user/src/main/java/cn/dankal/user/login/LoginActivity.java
@@ -0,0 +1,21 @@
+package cn.dankal.user.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.dankal.basiclib.base.activity.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+
+
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initComponents() {
+
+    }
+}
